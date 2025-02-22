47098d017892f6f796e2e3396c0aadea6c08581d
Mattia Maurizio <maurizio.mattia91@gmail.com>
2025-02-22 14:25:24 +0100

    fix user e auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { User } from '../user/user.entity';
+import { User } from './user/user.entity';
 
 @Module({
   imports: [
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../user/user.entity';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-  async generateTokens(user: any) {
-    const payload = { username: user.profile.username, sub: user.profile.id };
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.userRepository.findOne({ where: { username } });
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id, role: user.role };
     return {
       access_token: this.jwtService.sign(payload),
-      refresh_token: this.jwtService.sign(payload, { expiresIn: '7d' }),
     };
   }
 }
